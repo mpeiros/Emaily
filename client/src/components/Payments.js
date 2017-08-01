10792c82b75138053f490bdadc702b080f3c0b8e
Max Peiros <mpeiros@gmail.com>
2017-07-31 19:16:36 -0700

    Update styling on Payment component

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -4,11 +4,17 @@ import StripeCheckout from 'react-stripe-checkout';
 class Payments extends Component {
   render() {
     return (
-      <StripeCheckout 
+      <StripeCheckout
+        name="Emaily"
+        description="$5 for 5 email credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">
+          Add Credits
+        </button>
+      </StripeCheckout>
     );
   }
 }
