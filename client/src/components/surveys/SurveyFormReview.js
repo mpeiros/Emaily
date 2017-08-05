fd60bb3da9d7261fd15a6212125f6e28c7ca2f83
Max Peiros <mpeiros@gmail.com>
2017-08-05 13:39:21 -0700

    Extract FIELDS out into formFields file

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const SurveyFormReview = ({ onCancel }) => {
+const SurveyFormReview = ({ onCancel, formValues }) => {
   return (
     <div>
       <h5>Please confirm your entries</h5>
@@ -14,4 +15,10 @@ const SurveyFormReview = ({ onCancel }) => {
   );
 };
 
-export default SurveyFormReview;
+function mapStateToProps(state) {
+  return {
+    formValues: state.form.surveyForm.values
+  };
+}
+
+export default connect(mapStateToProps)(SurveyFormReview);
